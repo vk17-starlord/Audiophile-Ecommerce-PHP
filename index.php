e1e73fb67bf4af2e80ebfd37d4f8421a1f717be8
vk17-starlord <khollam.vinit@gmail.com>
2021-05-01 18:03:39 +0530

    added homepage

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Audiophile</title>
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<link rel="stylesheet" href="./main.css">
+</head>
+<body>
+    
+<div class="hero">
+<div class="container">
+<nav class="transparent z-depth-0">
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">audiophile</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+      <i class='bx bx-menu-alt-left'></i>
+      </a>
+      <ul class="right hide-on-med-and-down">
+        <li><a id="home-section" >Home</a></li>
+        <li><a id="headphones-section" >Headphones</a></li>
+        <li><a id="speakers-section" >Speakers</a></li>
+        <li><a id="earphones-section" >Earphones</a></li>
+        <li><a href=""><i class='bx bxs-cart-alt'></i></a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <ul class="sidenav" id="mobile-demo">
+  <li><a id="home-section" >Home</a></li>
+       
+        <li><a id="speakers-section" >Speakers</a></li>
+        <li><a id="earphones-section" >Earphones</a></li>
+        <li><a href=""><i class='bx bxs-cart-alt'></i></a></li>
+  </ul>
+
+
+  <div class="row">
+  <div class="col s12 m12 l6" id="hero-txt">
+  <span>NEW PRODUCT</span>
+  <h1 class="heading">
+  XX99 MARK II HEADPHONES
+  </h1>
+  <p>Experience natural , lifelike audio and exceptional build quality made for the passionate enthusiast</p>
+  <button class="cta-button">SEE PRODUCT <i class='bx bx-caret-right'></i></button>
+  </div>
+  <div class="col s12 m12 l6" id="hero-img">
+  </div>
+
+  </div>
+</div>
+</div>
+
+
+
+<!-- product cards section -->
+<div class="container">
+<div class="product-container row">
+<div class="col s12 m6 l4">
+<div class="p-card">
+<img src="./images/earphones.png" alt="">
+<h2>EARPHONES</h2>
+<a href="">shop <i class='bx bx-chevron-right'></i></a>
+</div>
+</div>
+<div class="col s12 m6 l4">
+<div class="p-card">
+<img src="./images/headphone1.png" alt="">
+<h2>HEADPHONES</h2>
+<a href="">shop <i class='bx bx-chevron-right'></i></a>
+</div>
+</div>
+<div class="col s12 m6 l4">
+<div class="p-card">
+<img src="./images/speaker.png" alt="">
+<h2>speaker</h2>
+<a href="">shop <i class='bx bx-chevron-right'></i></a>
+</div>
+</div>
+
+
+
+</div>
+</div>
+<div class="container">
+<div class="speaker-hero row">
+<div class="col s12 m6 l6 text">
+    <h2 class="heading">ZX9 <br> Speaker </h2>
+    <p>Upgrade to premium speaker that are phenomenally built to deliver truly remarkable sound</p>
+<button class="cta-button">SEE product</button>
+</div>
+<div class="col s12 m6 l6 img">
+<img src="./images/speakerpremium.webp" alt="">
+</div>
+</div>
+
+<div class="z-speaker">
+    <div class="p-heading">Z x speaker</div>
+    <button class="p-btn">see product</button>
+</div>
+
+<div class="y-headphone row">
+<div class="col s12 l6 m6">
+    <div class="y-card img"></div>
+</div>
+<div class="col s12 l6 m6">
+    <div class="y-card">
+    <h2 class="p-heading">YX1 Speaker</h2>
+    <button class="p-btn">see product</button>
+    </div>
+</div>
+
+</div>
+
+<div class="exp-container row">
+<div class="col s12 m12 l6">
+    <h2 class="p-heading">BRinging you the <span>best</span> audio gear</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo vero recusandae facere odit voluptates veniam quo cum numquam repellendus, laborum, eos porro repellat impedit reprehenderit aliquam qui voluptas at iusto possimus asperiores quia nesciunt odio, consectetur ut. Harum sint commodi corporis doloribus doloremque delectus, debitis, voluptates perspiciatis nesciunt illum libero!</p>
+</div>
+<div class="col s12 m12 l6 img">
+    
+</div>
+
+</div>
+</div>
+
+<footer>
+    <div class="container">
+    <nav class="transparent z-depth-0">
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">audiophile</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+      <i class='bx bx-menu-alt-left'></i>
+      </a>
+      <ul class="right hide-on-med-and-down">
+        <li><a id="home-section" >Home</a></li>
+       
+        <li><a id="speakers-section" >Speakers</a></li>
+        <li><a id="earphones-section" >Earphones</a></li>
+        <li><a href=""><i class='bx bxs-cart-alt'></i></a></li>
+      </ul>
+    </div>
+  </nav>  
+  <div class="row">
+      <div class="col s12 l6 m6">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet corrupti dolores fugiat debitis ullam repellendus optio quaerat, natus repudiandae reiciendis quis nostrum possimus id quam asperiores, dolor perferendis recusandae saepe.</p>
+      </div>
+      <div class="col s12 l6 m6">
+    <div>  <i class='bx bxl-instagram'></i>
+      <i class='bx bxl-facebook-circle' ></i>
+      <i class='bx bxl-twitter' ></i>
+      <i class='bx bxl-twitch' ></i></div>
+    </div>
+  </div> 
+    </div>
+</footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script>
+M.AutoInit();
+</script>
+</body>
+</html>
